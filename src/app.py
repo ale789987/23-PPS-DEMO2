2cc85f60315c3b8e2e9ae68efad8a41ec1f4e842
Ale <ale.esp.ap@gmail.com>
2024-05-08 19:15:57 +0200

    modifico app.py para forzar devops

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@
 # - con Heroku CLI
 # - sin usar GitHub
 #   Aunque luego al final lo manda a GitHub
-
+#otra cosa
 from flask import Flask, render_template
 from datetime import datetime
 import locale
